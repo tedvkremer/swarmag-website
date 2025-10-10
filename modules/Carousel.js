82f32d73e3fb2bcd84eda0c29790871e4c42290d
tedvkremer <tedvkremer@gmail.com>
2025-10-10 16:19:53 -0500

    Started gallery

---
+++ modules/Carousel.js
@@ -0,0 +1,92 @@
+const $ = (selector, node = document) => node.querySelector(selector);
+const $$ = (selector, node = document) => node.querySelectorAll(selector);
+
+/*************************************
+  Carousel
+ *************************************/
+
+export default class Carousel {
+  static DURATION = 8000;
+
+  #id = 0;
+  #slides = [];
+  #indicators = [];
+  #curr = 0;
+  #total = 0;
+  #interval = null;
+
+  constructor(id) {
+    this.#id = id;
+  }
+
+  init() {
+    const carousel = $(this.#id);
+    this.#slides = $$('.CarouselSlide', carousel);
+    this.#total = this.#slides.length;
+    this.#indicators = $$('.CarouselIndicators>button.CarouselDot', carousel);
+    this.#indicators.forEach((indicator, i) =>
+      indicator.addEventListener('click', () => this.toSlide(i))
+    );
+
+    const nextBtn = $('.CarouselNextSlide', carousel);
+    const prevBtn = $('.CarouselPrevSlide', carousel);
+    nextBtn.addEventListener('click', () => this.nextSlide());
+    prevBtn.addEventListener('click', () => this.prevSlide());
+
+    this.#updateCarousel();
+    this.startCarousel();
+  }
+
+  #updateCarousel() {
+    let offset = (0 - this.#curr) * 100;
+    this.#slides.forEach(slide => {
+      slide.style.transform = `translateX(${offset}%)`;
+      offset += 100;
+    });
+
+    this.#indicators.forEach((indicator, i) => {
+      if (this.#curr === i) {
+        indicator.classList.remove('CarouselDot');
+        indicator.classList.add('CarouselDotActive');
+        indicator.setAttribute('aria-current', 'true');
+      } else {
+        indicator.classList.remove('CarouselDotActive');
+        indicator.classList.add('CarouselDot');
+        indicator.removeAttribute('aria-current');
+      }
+    });
+  }
+
+  nextSlide() {
+    this.stopCarousel();
+    this.#curr = (this.#curr + 1) % this.#total;
+    this.#updateCarousel();
+    this.startCarousel();
+  }
+
+  prevSlide() {
+    this.stopCarousel();
+    this.#curr = (this.#curr - 1 + this.#total) % this.#total;
+    this.#updateCarousel();
+    this.startCarousel();
+  }
+
+  toSlide(index) {
+    if (index < 0 || index >= this.#total) return;
+    this.stopCarousel();
+    this.#curr = index;
+    this.#updateCarousel();
+    this.startCarousel();
+  }
+
+  startCarousel() {
+    if (this.#interval) return;
+    this.#interval = setInterval(() => this.nextSlide(), Carousel.DURATION);
+  }
+
+  stopCarousel() {
+    if (!this.#interval) return;
+    clearInterval(this.#interval);
+    this.#interval = null;
+  }
+}
--- modules/galleries.js
+++ modules/galleries.js
@@ -1,16 +1,68 @@
+import Carousel from './Carousel.js'
+
 const $ = (selector, node = document) => node.querySelector(selector);
 const $$ = (selector, node = document) => node.querySelectorAll(selector);
 
+const GALLERIES = {
+  "gallery-areal": [
+    "../galleries/g1-photo-1.jpg",
+    "../galleries/g1-photo-2.jpg",
+    "../galleries/g1-photo-3.jpg",
+    "../galleries/g1-photo-4.jpg",
+    "../galleries/g1-photo-5.jpg",
+    "../galleries/g1-photo-6.jpg",
+  ],
+  "gallery-ground": [
+    "../galleries/g2-photo-1.jpg",
+    "../galleries/g2-photo-2.jpg",
+    "../galleries/g2-photo-3.jpg",
+    "../galleries/g2-photo-4.jpg",
+    "../galleries/g2-photo-5.jpg",
+    "../galleries/g2-photo-6.jpg",
+  ]
+};
+
 export const init = () => initGalleries();
+
+/*************************************
+  Galleries
+ *************************************/
 
 function initGalleries() {
   $$('.Gallery').forEach(g => initGallery(g));
 }
 
-/*************************************
-  Gallery Carousel
- *************************************/
-
 function initGallery(gallery) {
+  const id = `#{gallery.id}.Carousel`;
+  const carousel = $(id, gallery);
+  const container = $('.CarouselContainer', carousel);
+  const indicators = $('.CarouselIndicators', carousel);
 
+  function createSlide(photo, pos) {
+    const slide = `
+      <div class="CarouselSlide" style="transform: translateX(${pos}%)">
+        <img class="CarouselSlideImage" src="${photo}" alt="swarmAg" />
+      </div>
+    `;
+    container.appendHTML(slide);
+  }
+
+  function createDot(ndx) {
+    const dot = `
+      <button class="CarouselDot" role="tab" aria-label="Go to slide ${ndx}" tabindex="-1"></button>
+    `;
+    indicators.appendHTML(dot);
+  }
+
+  function createPhoto(photo, pos, ndx) {
+    createSlide(photo, pos);
+    createDot(ndx);
+  }
+
+  let pos = 0, ndx = 0;
+  GALLERIES[gallery.id].forEach(
+    photo => createPhoto(photo, pos += 100, ndx++)
+  );
+
+  new Carousel(id).init();
 }
