78309f4e4671f8aad6f610e518cde785bb093fd7
Ted V. Kremer <tedvkremer@gmail.com>
2025-11-04 13:15:46 -0600

    Clean-up

--- modules/PhotoCatalog.js
+++ modules/PhotoCatalog.js
@@ -16,6 +16,8 @@ export default class PhotoCatalog {
       "../galleries/areal/g1-photo-04.jpg",
       "../galleries/areal/g1-photo-05.jpg",
       "../galleries/areal/g1-photo-06.jpg",
+      "../galleries/areal/g1-photo-07.jpg",
+      "../galleries/areal/g1-photo-08.jpg",
     ], 5)),
     "gallery-ground": immutable(shuffle([
       "../galleries/ground/g2-photo-00.jpg",
